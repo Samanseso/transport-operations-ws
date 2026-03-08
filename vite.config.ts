faa49891162ad3063fa6ea62dd6863715c1ea23b
Samanseso <winesevander@gmail.com>
2026-03-09 02:44:57 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig ,} from 'vite';
 
 export default defineConfig({
-    base: '/assets/',
+    base: 'https://transport-operations-software.onrender.com/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],

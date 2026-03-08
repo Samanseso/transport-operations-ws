753d1ff6bb3432bb948582d5f030be809958b692
Samanseso <winesevander@gmail.com>
2026-03-09 01:09:24 +0800

    update

--- database/migrations/2026_03_09_002707_create_password_reset_tokens_table.php
+++ database/migrations/2026_03_09_002707_create_password_reset_tokens_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetTokensTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreatePasswordResetTokensTable extends Migration
     {
         Schema::dropIfExists('password_reset_tokens');
     }
-}
+};

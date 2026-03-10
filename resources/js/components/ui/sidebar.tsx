f347c562ec0535432d1c6caa48c328028ec10171
Samanseso <winesevander@gmail.com>
2026-03-10 12:49:21 +0800

    update

--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -173,6 +173,8 @@ function SidebarProvider({
 	const isMobile = useIsMobile()
 	const [openMobile, setOpenMobile] = React.useState(false)
 
+	console.log(auth);
+
 	// This is the internal state of the sidebar.
 	// We use openProp and setOpenProp for control from outside the component.
 	const [_open, _setOpen] = React.useState(defaultOpen)

991a384656ee676c1f5a9da27099e4c0bc9a8a78
Samanseso <winesevander@gmail.com>
2026-03-10 10:59:46 +0800

    update

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -5,13 +5,13 @@ declare global {
 		Echo: Echo<any>;
 	}
 }
+
 window.Echo = new Echo({
-	broadcaster: 'reverb',
+	broadcaster: "reverb",
 	key: import.meta.env.VITE_REVERB_APP_KEY,
 	wsHost: import.meta.env.VITE_REVERB_HOST,
 	wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
 	wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-	forceTLS: false,
-	enabledTransports: ['ws', 'wss'],
-})
-
+	forceTLS: true, // IMPORTANT
+	enabledTransports: ["ws", "wss"],
+});
